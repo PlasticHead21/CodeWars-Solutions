ed4eb25cfbb4436a3198fbdf1558feea8f2b41b8
Denis M <plastichead23@gmail.com>
2019-01-29 21:38:30 +0300

    Completed solution

--- WeightForWeight/Program.cs
+++ WeightForWeight/Program.cs
@@ -12,32 +12,17 @@ namespace WeightForWeight
         {
             Console.WriteLine(WeightSort.OrderWeight("103 123 4444 99 2000 180"));
             Console.WriteLine(WeightSort.OrderWeight("11 11 2000 10003 22 123 1234000 44444444 9999"));
+            Console.WriteLine(WeightSort.OrderWeight("11 2 3 111 1111 4 22"));
         }
     }
     public class WeightSort
     {   
         public static string OrderWeight(string str)
-        {            
-            string[] strArr = str.Split(new char[] { ' ' });
-            List<int> digits = new List<int>();
-            List<Weight> weightList = new List<Weight>();
-            string res = default(string);
-            for (int index = 0; index < strArr.Length; index++)
-            {
-                for (int i = 0; i < strArr[index].Length; i++)
-                {
-                    digits.Add(Int32.Parse(strArr[index][i].ToString()));
-                }
-                
-                weightList.Add(new Weight { Digit = digits.Sum(), Svalue = String.Join("", digits) });
-                digits.Clear();
-            }
-            weightList = weightList.OrderBy(x => x.Digit).ToList();
-            foreach (var item in weightList)
-            {
-                res += item.Svalue + " ";
-            }
-            return res.Trim(new char[] { ' ' });
+        {
+            return string.Join(" ", str.Split(' ')
+            .OrderBy(n => n.ToCharArray()
+            .Select(c => (int)char.GetNumericValue(c)).Sum())
+            .ThenBy(n => n));
         }
     }
 }
