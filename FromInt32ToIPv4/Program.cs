f1f0cbc70c6a04a6c92d13ff2bfe7b8e20142f20
PlasticHead21 <plastichead23@gmail.com>
2019-03-09 14:40:04 +0300

    Added FromInt32ToIPv4 solution

---
+++ FromInt32ToIPv4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromInt32ToIPv4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.UInt32ToIP(2154959208);
+            Kata.UInt32ToIPLinq(2149583361);
+        }   
+    }
+
+    public static class Kata
+    {
+        public static string UInt32ToIP(uint ip)
+        {
+            byte[] binary = BitConverter.GetBytes(ip);            
+            return String.Join(".", binary.Reverse());
+        }
+
+        public static string UInt32ToIPLinq(uint ip)
+          => string.Join(".", (new int[] { 24, 16, 8, 0 }).Select(e => ip >> e & 255));
+    }
+}
