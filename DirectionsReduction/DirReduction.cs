c9b228ef79e4f467a526a8563af50a3868180ea9
PlasticHead21 <PlasticHead23@gmail.com>
2019-10-20 18:03:29 +0300

    Create solution of "Directions Reduction" kata

---
+++ DirectionsReduction/DirReduction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectionsReduction
+{
+    internal class DirReduction
+    {
+        public static String[] Reduction(String[] arr)
+        {
+            if (arr is null)
+                return arr;
+
+            Stack<string> st = new Stack<string>();
+            foreach (var item in arr)
+            {   
+
+                switch (item)
+                {
+                    case "NORTH":
+                        if(st.Count != 0 && st.Peek() == "SOUTH")
+                        {
+                            st.Pop();
+                        }
+                        else
+                        {
+                            st.Push(item);
+                        }
+                        break;
+
+                    case "SOUTH":
+                        if (st.Count != 0 && st.Peek() == "NORTH")
+                        {
+                            st.Pop();
+                        }
+                        else
+                        {
+                            st.Push(item);
+                        }
+                        break;
+
+                    case "WEST":
+                        if (st.Count != 0 && st.Peek() == "EAST")
+                        {
+                            st.Pop();
+                        }
+                        else
+                        {
+                            st.Push(item);
+                        }
+                        break;
+
+                    case "EAST":
+                        if (st.Count != 0 && st.Peek() == "WEST")
+                        {
+                            st.Pop();
+                        }
+                        else
+                        {
+                            st.Push(item);
+                        }
+                        break;
+
+                    default:
+                        st.Push(item);
+                        break;
+                }
+            }
+            arr = st.ToArray();
+            Array.Reverse(arr);
+            return arr;
+        }
+    }
+}
---
+++ DirectionsReduction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DirectionsReduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+            string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            string[] c = new string[] { "NORTH" };
+            string[] e = { "EAST", "EAST", "WEST", "NORTH", "WEST", "EAST", "EAST", "SOUTH", "NORTH", "WEST" };
+            string[] d = { "NORTH", "EAST", "NORTH", "EAST", "WEST", "WEST", "EAST", "EAST", "WEST", "SOUTH" };
+
+
+            Array.ForEach(DirReduction.Reduction(d), (element) => Console.WriteLine(element));
+        } 
+    }
+}
