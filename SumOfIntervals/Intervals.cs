0888c08bfa1d086d62c4e0b7cf37515a62507769
Denis Martynenko <denis.martynenko93@gmail.com>
2019-10-27 20:18:39 +0300

    Sum of intervals

---
+++ SumOfIntervals/Intervals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SumOfIntervals
+{
+    internal class Intervals
+    {
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            List<int> resultArr = new List<int>();
+            foreach (var interval in intervals)
+            {
+                for (int i = interval.Item1; i < interval.Item2; i++)
+                {
+                    resultArr.Add(i);
+                }
+            }
+            return resultArr.Distinct().Count();
+        }
+    }
+}
