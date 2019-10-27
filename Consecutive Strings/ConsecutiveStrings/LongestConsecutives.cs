7c293168c99d577ea8096260be977585a6a110d5
PlasticHead21 <PlasticHead23@gmail.com>
2019-10-27 19:21:23 +0300

    Consecutive Strings
    This reverts commit 3531bbf71e3aac9a7778164a08aba7c391ea1acd.

---
+++ Consecutive Strings/ConsecutiveStrings/LongestConsecutives.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsecutiveStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3));
+        }
+    }
+
+    public class LongestConsecutives
+    {
+        public static String LongestConsecLinq(string[] s, int k)
+        {
+            return s.Length == 0 || s.Length < k || k <= 0 ? ""
+              : Enumerable.Range(0, s.Length - k + 1)
+                          .Select(x => string.Join("", s.Skip(x).Take(k)))
+                          .OrderByDescending(x => x.Length)
+                          .First();
+        }
+
+        public static string LongestConsec(string[] strarr, int k)
+        {
+            if (k > strarr.Length || strarr.Length == 0 || k <= 0)
+            {
+                return string.Empty;
+            }
+
+            var currentStr = string.Empty;
+            for (var i = 0; i < strarr.Length; i++)
+            {
+                var str = string.Empty;
+                for (var j = i; j < k + i && j < strarr.Length; j++)
+                {
+                    str += strarr[j];
+                }
+
+                if (currentStr.Length < str.Length)
+                {
+                    currentStr = str;
+                }
+            }
+
+            return currentStr;
+        }
+
+    }
+}
